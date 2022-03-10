f5c205e45a67d202f2fe080e306d16c9013f4d4d
SamFuentes <sam.fuentesg@gmail.com>
2022-03-09 20:53:55 -0600

    Fix error of api call

--- src/Weather.js
+++ src/Weather.js
@@ -4,60 +4,66 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [city, setCity] = useState(" ");
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState({ ready: false });
 
   function handleResponse(response) {
     setCity(props.city);
     setWeather({
+      ready: true,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
-      wind: response.data.main.speed,
+      wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
-
-  let apiKey = "226ed7484bfdb5ee56b0b7f78f0f94e0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&unit=metrics`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Weather">
-      <div className="row">
-        <div className="col-9">
-          <form>
+  if (weather.ready) {
+    return (
+      <div className="Weather">
+        <div className="row">
+          <div className="col-9">
+            <form>
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Enter a city..."
+              />
+            </form>
+          </div>
+          <div className="col-1">
             <input
-              type="search"
-              className="form-control"
-              placeholder="Enter a city..."
-            />
-          </form>
-        </div>
-        <div className="col-1">
-          <input
-            type="button"
-            value="Search"
-            className="btn btn-outline-light"
-          ></input>
-        </div>
-      </div>
-      <h1 className="pt-5 pb-4">City, Country</h1>{" "}
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-6">
-            <div>{Math.round(weather.temperature)}°C </div>
-            <img src={weather.icon} alt="Weather Icon" />
+              type="button"
+              value="Search"
+              className="btn btn-outline-light"
+            ></input>
           </div>
-          <div className="col-6">
-            <ul>
-              {" "}
-              <li> Description: {weather.description} </li>
-              <li> Humidity: {weather.humidity}%</li>
-              <li> Wind: {weather.wind}km/h </li>
-            </ul>
+        </div>
+        <h1 className="pt-5">City, Country</h1>{" "}
+        <p className="text-capitalize">{weather.description} </p>
+        <div className="container">
+          <div className="row d-flex align-items-center">
+            <div className="col tempInfo">
+              <img src={weather.icon} alt="Weather Icon" />
+              <span className="temperature">
+                {Math.round(weather.temperature)}°C{" "}
+              </span>{" "}
+            </div>
+            <div className="col">
+              <ul>
+                {" "}
+                <li> Humidity: {weather.humidity}%</li>
+                <li> Wind: {weather.wind}km/h </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "a410057f698a69a787c08a64653ca0de";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return <h1>Loading...</h1>;
+  }
 }
