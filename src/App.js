6dda2d0f95f3e1782908a61f875b40c81fb0d6a4
SamFuentes <sam.fuentesg@gmail.com>
2022-04-15 16:11:55 -0500

    Update links

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <div className="container">
         <Weather city="vancouver" />
         <footer>
-          This proyect was code by <a href="">Sam Fuentes</a>,{""} and is {""}
+          This proyect was code by{" "}
+          <a href="https://www.samfuentesg.com/">Sam Fuentes</a>,{""} and is{" "}
+          {""}
           <a
             href="https://github.com/Samfuentes/weather-react"
             target="_blank"
